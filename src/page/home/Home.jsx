ae4eb8c9cf69a35406a90a508b9f3bede152a6d9
Nimai360 <nimai360@hotmail.com>
2023-11-04 22:33:39 -0300

    Login e Cadastro pronto. Fazendo Card Produto

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
+import Card from "../../components/cardProduct/CardProduct";
 
 export default function Home(){
     return(
-        <h1>Home</h1>
+        <>
+            <h1>Home</h1>
+            <div className="App">
+                <div className="container" >
+                    <Card />
+                </div>
+            </div>
+        </>
     )
 }
