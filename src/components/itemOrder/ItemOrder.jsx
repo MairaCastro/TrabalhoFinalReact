4fe10da1f69a451e66e5f6d96b350162e84c7628
Nimai360 <nimai360@hotmail.com>
2023-11-07 04:33:21 -0300

    Passando infor pro cart

--- src/components/itemOrder/ItemOrder.jsx
+++ src/components/itemOrder/ItemOrder.jsx
@@ -3,43 +3,48 @@ import { Link, Navigate, useNavigate } from 'react-router-dom';
 // import "./Card.css";
 import styled from 'styled-components';
 
-export default function CardProduct( product ) {
-  const [description, setDescription] = useState( '');
-  const [title, setTitle] = useState('');
-  const [preco, setPreco] = useState('');
+export default function CardProduct( pedido ) {
+      const [produto, setProdutos] = useState(null);
+//   const [description, setDescription] = useState( '');
+//   const [title, setTitle] = useState('');
+//   const [preco, setPreco] = useState('');
 
-  useEffect(() => {
-    setDescription(product.product.descricao)
-    setTitle(product.product.nome)
-    setPreco(FormattedNumber(product.product.preco))
-  }, [product]);
+//   useEffect(() => {
+//     setDescription(product.product.descricao)
+//     setTitle(product.product.nome)
+//     setPreco(FormattedNumber(product.product.preco))
+//   }, [pedido]);
  
-  useEffect(() => {
-    const maxTam = 27
-    if(description.length > maxTam){
-      setDescription(description.substring(0, maxTam-3) + '...');
-    }
-  }, [description]);
+//   useEffect(() => {
+//     const maxTam = 27
+//     if(description.length > maxTam){
+//       setDescription(description.substring(0, maxTam-3) + '...');
+//     }
+//   }, [description]);
 
-  useEffect(() => {
-    const maxTam = 28
-    if(title.length > maxTam){
-      setTitle(title.substring(0, maxTam-3) + '...');
-    }
-  }, [title]);
+//   useEffect(() => {
+//     const maxTam = 28
+//     if(title.length > maxTam){
+//       setTitle(title.substring(0, maxTam-3) + '...');
+//     }
+//   }, [title]);
 
-  function FormattedNumber( number ) {
-    const formattedNumber = number?.toLocaleString('pt-BR', { //usando ?. para chamar toLocaleString() apenas se number não for undefined ou null. Se number for undefined ou null, formattedNumber será undefined
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2
-    });
+//   function FormattedNumber( number ) {
+//     const formattedNumber = number?.toLocaleString('pt-BR', { //usando ?. para chamar toLocaleString() apenas se number não for undefined ou null. Se number for undefined ou null, formattedNumber será undefined
+//       minimumFractionDigits: 2,
+//       maximumFractionDigits: 2
+//     });
 
-    return formattedNumber
-  }
+//     return formattedNumber
+//   }
+
+// console.log("ItemOrder:", pedido)
 
   return (
     <>
-      <h1>oi</h1>
+        <div>
+            <h1>{'oi'}</h1>
+        </div>
     </>
   );
 }
--- src/components/productOrder/ProductOrder.jsx
+++ src/components/productOrder/ProductOrder.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import ItemOrder from '../../components/itemOrder/ItemOrder';
 import styled from 'styled-components';
+import http from '../../components/conexaoDb/ConexaoDb';
 
 export default function ProductOrder({ pedidos }) {
   // const [products, setProduct] = useState([]);
 
   useEffect(() => {
-    console.log(pedidos)
+    // console.log(pedidos)
   }, []);
 
   const Lista = styled.div`
@@ -20,14 +21,33 @@ export default function ProductOrder({ pedidos }) {
     justify-content: center;
   `;
 
+const getProdutosDB = async (idproduto) => {
+    const response = await http.get('/produtos');
+    return response.data;
+  };
+  
+  
+
+  useEffect(() => {
+    // console.log(pedidos)
+    pedidos.forEach(async (pedido) => {
+      const produtos = await getProdutosDB();
+      const produto = produtos.find(produto => produto.id === pedido.idproduto);
+      pedido.produto = produto;
+    //   console.log("pedido.produto", pedido.produto)
+    //   console.log("pedido.produto2", pedido)
+      return pedido
+    });
+  }, []);
+  
+
   return (
     <>
-    {/* {pedidos} */}
-      {/* <Lista>
+      <Lista>
         {pedidos.map((pedido) => (
-            <ItemOrder key={pedido.id} product={pedido} />
+            <ItemOrder key={pedido.id} pedido={pedido} produto={pedido.produto} />
         ))}
-      </Lista> */}
+      </Lista>
     </>
   );
 }
